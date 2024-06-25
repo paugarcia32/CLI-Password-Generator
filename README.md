b7c09e5521ad313692d9b82fda1bd3c34643f874
Pau Garcia <paugarcia32@gmail.com>
2024-06-25 15:30:25 +0200

    strcut and arguments added

--- README.md
+++ README.md
@@ -3,12 +3,26 @@
 The aim of this project is to create a fully random CLI Password Generator.
 
 Goals:
+First version
 
 - [x] build a logic to generate a password
 - [x] testing that everything works
+      Seccond version
+- [x] Adding the structopt crate
+- [x] Use struct opt in the main.rs
+- [x] Bind and config the arguments expected from the end user
+- [x] Test
 
 # How to run the appication
 
+## Default Mode
+
 ```rust
 cargo run
+```
+
+## With arguments
+
+```rust
+cargo r -- --length 12
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use rand::seq::SliceRandom;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "Password generator", about = "A strong password generator")]
+struct Arguments {
+    #[structopt(long = "length", short = "l", default_value = "8")]
+    length: usize,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::from_args();
 
-    let length: usize = 12;
+    println!("Length is: {}", args.length);
+
     let char_pool: Vec<char> =
         "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-="
             .chars()
             .collect();
 
-    let password: String = (0..length)
+    let password: String = (0..args.length)
         .map(|_| *char_pool.choose(&mut rand::thread_rng()).unwrap())
         .collect();
 
